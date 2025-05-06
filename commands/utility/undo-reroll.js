e90cac1e7340bd8ea27323c484c5e1db77f704ac
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-05-06 21:23:50 +0200

    Remove public announcement in undo reroll

--- commands/utility/undo-reroll.js
+++ commands/utility/undo-reroll.js
@@ -14,13 +14,8 @@ module.exports = {
                 .setRequired(true)),
 
     async execute(interaction) {
-        let dutyChannel = interaction.client.channels.cache.get('972537907732684880');
         let dutyList = rerollDuty(getDutyList());
-        const reason = interaction.options.getString('reason');
 
-        dutyChannel.send(`<@${dutyList[0]}> má tento týden službu!
--# Službu un-přesločil <@${interaction.user.id}> pomocí příkazu. Člověk, který byl přeskočen, bude mít službu tento týden.
--# Důvod: \`${reason}\``);
         await interaction.reply({ content: `Služba byla un-přeskočena.
 Nový pořadník: ${getStringList(dutyList)}`, flags: MessageFlags.Ephemeral });
     },
