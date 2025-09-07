572a031da58d339eea101da2347888cf9239eb1e
Antonin <81852696+Limit-sest@users.noreply.github.com>
2025-09-07 22:20:29 +0200

    Add missing import

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 import cron from 'node-cron';
 import {completeDuty, getDutyList} from "./utils/duty.js";
 const { token } = require('./config.json');
